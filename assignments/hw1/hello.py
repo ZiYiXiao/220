3ef50c905ac5d3d53a19517185948b51f6cf6d52
ziyixiao <xiaoz1@g.cofc.edu>
2021-09-06 12:30:13 -0400

    Completed hw1

--- assignments/hw1/hello.py
+++ assignments/hw1/hello.py
@@ -2,10 +2,15 @@
 Name: Zi Yi Xiao
 hello.py
 
-Problem: This function prints "Hello, World!" to the screen.
+Problem: This function prints "hello, world!" to the screen.
 
 Certification of Authenticity:
 I certify that this assignment is entirely my own work.
 """
 
-print("Hello, World!")
+
+def main():
+    print("hello, world!")
+
+
+main()
