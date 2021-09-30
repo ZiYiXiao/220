3c3a595d2524f2b689ddc7d7618a2bea2f3b6df7
ZiYiXiao <89540110+ZiYiXiao@users.noreply.github.com>
2021-09-30 01:56:37 -0400

    Completed lab 5

--- labs/lab5/lab5.py
+++ labs/lab5/lab5.py
@@ -1,21 +1,14 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Zi Yi Xiao
+lab5.py
+
+Problem: This program writes various functions.
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
 """
 
 from graphics import *
-
-
-def target():
-    win_width = 500
-    win_height = 500
-    win = GraphWin("Archery Target", win_width, win_height)
-
-    # Add code here to get the dimensions and draw the target
-
-    # Wait for another click to exit
-    win.getMouse()
-    win.close()
 
 
 def triangle():
@@ -23,10 +16,23 @@ def triangle():
     win_height = 500
     win = GraphWin("Draw a Triangle", win_width, win_height)
 
-    # Add code here to accept the mouse clicks, draw the triangle.
-    # and display its area in the graphics window.
+    p1 = win.getMouse()
+    p2 = win.getMouse()
+    p3 = win.getMouse()
+    tri = Polygon(p1, p2, p3)
+    tri.draw(win)
+    side1 = abs(p1.getX() - p2.getX())
+    side2 = abs(p2.getX() - p3.getX())
+    side3 = abs(p1.getX() - p3.getX())
+    perimeter = side1 + side2 + side3
+    area = (side1 * side2 * side3) / 2
+    inst_pt = Point(500 / 2, 500 - 10)
+    instructions = Text(inst_pt, "The area is: " + str(area))
+    instructions.draw(win)
+    inst_pt = Point(500 / 2, 500 - 30)
+    instructions = Text(inst_pt, "The perimeter is: " + str(perimeter))
+    instructions.draw(win)
 
-    # Wait for another click to exit
     win.getMouse()
     win.close()
 
@@ -71,16 +77,68 @@ def color_shape():
     green_text.draw(win)
     blue_text.draw(win)
 
+    red_box = Entry(Point(200, 240), 5)
+    green_box = red_box.clone()
+    green_box.move(0, 30)
+    blue_box = red_box.clone()
+    blue_box.move(0, 60)
+    red_box.draw(win)
+    green_box.draw(win)
+    blue_box.draw(win)
+    for i in range(5):
+        win.getMouse()
+        red = int(red_box.getText())
+        green = int(green_box.getText())
+        blue = int(blue_box.getText())
+        color = color_rgb(red, green, blue)
+        shape.setFill(color)
+
     # Wait for another click to exit
     win.getMouse()
     win.close()
 
 
+def process_string():
+    s = eval(input("Input a string: "))  # Input a string: 'Hello world'
+    print(s[0])
+    print(s[len(s) - 1])
+    print(s[2:6])
+    print(s[::len(s) - 1])
+    print(s[0:3] * 10)
+    for i in range(len(s)):
+        print(s[i])
+    print(len(s))
+
+
+def process_list():
+    pt = Point(5, 10)
+    values = [5, "hi", 2.5, "there", pt, "7.2"]
+    print("x =", values[1] + values[3])
+    print("x =", values[0] + values[2])
+    print("x =", values[1] * 5)
+    print("x =", values[2:5])
+    print("x =", values[2], ",", values[3], ",", values[0])
+    print("x =", values[2], ",", values[0], ",", values[5])
+    print("x =", values[0] + values[2] + float("7.2"))
+    print("x =", str(len(values)))
+
+
+def another_series():
+    acc = 0
+    series = eval(input("Input number of terms: "))  # Input number of terms: 10
+    for i in range(series):
+        y = 2 + 2 * (i % 3)
+        print(y, end=" ")
+        acc = acc + y
+    print("\nSum = " + str(acc))
+
+
 def main():
-    # target()
-    # triangle()
+    triangle()
     # color_shape()
-    pass
+    # process_string()
+    # process_list()
+    # another_series()
 
 
 main()
